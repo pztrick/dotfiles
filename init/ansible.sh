b0e12d64c254991ea3f90e1f180717c3dff0e579
Patrick Paul <pztrick44@gmail.com>
2014-07-23 18:33:22 -0400

    ignore more

--- init/ansible.sh
+++ init/ansible.sh
@@ -1,2 +1,3 @@
 ln -s ~/.files/ansibleconfig ~/.ansible.cfg
-ln -s ~/.files/ansible/plugins/inventory/ec2.py ~/ec2.py
+ln -s ~/.files/ansible/plugins/inventory/ec2.py ~/ansible_hosts
+ln -s ~/.files/ansibleconfig /etc/ansible/ec2.ini
