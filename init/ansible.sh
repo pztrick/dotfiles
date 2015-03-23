b456492856b88bda960e7682e86c86e9f1c81d62
Patrick Paul <pztrick44@gmail.com>
2015-03-23 10:32:12 -0400

    Adds pipx alias

--- init/ansible.sh
+++ init/ansible.sh
@@ -1,3 +1,4 @@
 ln -s ~/.files/ansibleconfig ~/.ansible.cfg
 ln -s ~/.files/ansible/plugins/inventory/ec2.py ~/ansible_hosts
 ln -s ~/.files/ansibleconfig /etc/ansible/ec2.ini
+cd ~/.files/ansible; sudo python setup.py install
