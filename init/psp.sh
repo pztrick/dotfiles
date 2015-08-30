af245708ff930dfe3140def0674b1b16afa1a69e
Patrick Paul <patrick@astrohaus.com>
2015-08-30 15:48:20 -0400

    adds flake8

--- init/psp.sh
+++ init/psp.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y python-software-properties python-pip python-yaml python-dev dsh keychain meld zsh tmux silversearcher-ag build-essential devscripts debhelper vim-gnome vim-gui-common equivs
+sudo apt-get install -y python-software-properties python-pip python-yaml python-dev dsh keychain meld zsh tmux silversearcher-ag build-essential devscripts debhelper vim-gnome vim-gui-common equivs python-flake8
