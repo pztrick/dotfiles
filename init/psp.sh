75420f3e4010506f743fc7b111422b4ef9c6102e
Patrick Paul <patrick@astrohaus.com>
2015-09-13 22:57:24 -0400

    Adds git-flow

--- init/psp.sh
+++ init/psp.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y python-software-properties python-pip python-yaml python-dev dsh keychain meld zsh tmux silversearcher-ag build-essential devscripts debhelper vim-gnome vim-gui-common equivs python-flake8
+sudo apt-get install -y python-software-properties python-pip python-yaml python-dev dsh keychain meld zsh tmux silversearcher-ag build-essential devscripts debhelper vim-gnome vim-gui-common equivs python-flake8 exuberant-ctags git-flow
