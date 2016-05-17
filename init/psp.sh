055817c00cbd1df587e505b974677f3afc5bab56
Patrick Paul <patrick@astrohaus.com>
2016-05-17 12:02:46 -0400

    Adds more deps to init.sh

--- init/psp.sh
+++ init/psp.sh
@@ -1,2 +1,4 @@
-sudo apt-get install -y python-software-properties python-pip python-yaml python-dev dsh keychain meld zsh tmux silversearcher-ag build-essential devscripts debhelper vim-gnome vim-gui-common equivs python-flake8 exuberant-ctags git-flow golang gom cmake libffi-dev libssl-dev
+sudo apt-get install -y python-software-properties python-pip python-yaml python-dev dsh keychain meld zsh tmux silversearcher-ag build-essential devscripts debhelper vim-gnome vim-gui-common equivs python-flake8 exuberant-ctags git-flow golang gom cmake libffi-dev libssl-dev htop libxml2-dev libxslt1-dev pandoc libpq-dev
+
+
 sudo apt-get install vim-gnome-py2 || echo 'ok'
