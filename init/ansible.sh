01e2498782f6e5b4a9d7c94d5fddc4d5e738040d
Patrick Paul <patrick@astrohaus.com>
2017-11-25 19:33:58 -0500

    updates packages

--- init/ansible.sh
+++ init/ansible.sh
@@ -1,3 +1,3 @@
 ln -sf ~/.files/ansibleconfig ~/.ansible.cfg
 ln -sf ~/.files/init/ansible-ec2.py ~/ansible_hosts
-sudo ln -sf ~/.files/ansibleconfig /etc/ansible/ec2.ini
+sudo ln -sf ~/.files/ansibleconfig /etc/ansible/ec2.ini || true
