9199b22542bd7de749b597491aa8fc523546a880
patrick <git@foosrank.com>
2020-04-25 21:26:28 -0400

    adds newren/git-filter-repo to bin

--- init/gitconfig.sh
+++ init/gitconfig.sh
@@ -1,2 +1,4 @@
 sudo rm -r ~/.gitconfig 2> /dev/null
 ln -s ~/.files/gitconfig ~/.gitconfig
+sudo wget https://raw.githubusercontent.com/newren/git-filter-repo/master/git-filter-repo -O /usr/local/bin/git-filter-repo
+sudo chmod +x /usr/local/bin/git-filter-repo
